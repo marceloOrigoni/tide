b765ee8ff5454cd96c6acce26a857f1d06e7bdc6
Marcelo Origoni <morigoni@mulesoft.com>
2022-08-06 20:48:21 -0300

    Added a new option to always show node version

--- functions/_tide_item_node.fish
+++ functions/_tide_item_node.fish
@@ -1,3 +1,7 @@
 function _tide_item_node
-    test -e package.json && _tide_print_item node $tide_node_icon' ' (node --version | string trim --chars=v)
+    if test "$tide_node_always_display" = true
+        _tide_print_item node $tide_node_icon' ' (node --version | string trim --chars=v)
+    else
+        test -e package.json && _tide_print_item node $tide_node_icon' ' (node --version | string trim --chars=v)
+    end
 end
